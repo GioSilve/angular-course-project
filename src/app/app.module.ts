1fcc57d529ebf7c4a6062639ce20fbc3dcaa2960
GioSilve <giorgiosilvestre01@gmail.com>
2025-09-17 11:27:39 +0200

    fix: optimized application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,9 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
-import { CreateCharacterComponent } from './create-character/create-character.component';
-import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
-const routes: Routes = [
-  {
-    path: 'characters',
-    component: TabsComponent,
-    children: [
-      { path: '', redirectTo: 'all', pathMatch: 'full' },
-      { path: ':side', component: ListComponent },
-    ],
-  },
-  { path: 'new-character', component: CreateCharacterComponent },
-  { path: '**', redirectTo: '/characters' },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -32,15 +17,9 @@ const routes: Routes = [
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent,
     HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent],
 })
